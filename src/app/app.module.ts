29fd5908996540fa84fd6ac862a33dea8a022fc6
yehchitsai <yehchitsai@hotmail.com>
2017-07-23 17:19:50 +0800

    1.練習pipe 2.練習 Angular 的 DI 與 服務元件 開發

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,22 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
 import { ArticleComponent } from './article/article.component';
+import { MaskPipe } from './mask.pipe';
+import { DataServiceService } from './data-service.service';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ArticleComponent
+    ArticleComponent,
+    MaskPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [DataServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,20 +1,18 @@
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { DataServiceService } from '../data-service.service';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
+
 export class ArticleComponent implements OnInit {
   @Input() item;
-  constructor() { }
-  @Output() delete = new EventEmitter<any>();
+  constructor(public dataSvr: DataServiceService) { }
 
   ngOnInit() {
   }
 
-  onDelete(){
-    this.delete.emit(this.item);
-  }
 
 }
